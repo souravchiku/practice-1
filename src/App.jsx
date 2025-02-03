f475687cf36d79675287e2e08a211c9e26272729
Sourav kumar <souravchiku0@gmail.com>
2025-02-03 10:15:57 +0530

    pagination implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
     fetchProducts();
@@ -16,18 +17,39 @@ export default function App() {
     }
   };
 
+  const selectedPage = (selectedPage)=>{
+    setPage(selectedPage)
+  }
   return (
     <div className="container">
       {products.length > 0 && (
         <div className="card-container">
-          {products.map((elm) => (
+          {products.slice(page * 10 - 10, page * 10).map((elm) => (
             <div key={elm.id} className="card">
               <img src={elm.thumbnail} alt={elm.title} width={100} height={100} />
               <span>{elm.title}</span>
             </div>
           ))}
+
+
+          <div>
+            {products.length > 0 &&
+              <div className="pagination">
+
+                <span className="pageButton">👈</span>
+                <span  className="pageButton">{[...Array(products.length/10)].map((_,i)=>{
+                  return <span className="pageButton" onClick={()=>selectedPage(i+1)}  key={i}>{i + 1}</span>
+                })}</span>
+                <span  className="pageButton">👉</span>
+              </div>
+            }
+          </div>
         </div>
+
+
       )}
+
+
     </div>
   );
 }
